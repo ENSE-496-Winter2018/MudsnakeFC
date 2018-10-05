85e19eb102912db5ad054003b38b65ea15db21ed
connor-meredith <connor.meredith1@gmail.com>
2018-10-04 19:39:46 -0600

    Added Controller and actions required for logon

---
+++ Source/WebApplication/WebApplication/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+namespace WebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User userParam)
+        {
+            using (var context = new ENSE496Entities())
+            {
+                var user = context.Users.Where(u => u.Username == userParam.Username).FirstOrDefault();
+                if (user.Hashed_password == userParam.Hashed_password)
+                    return RedirectToAction("Index");
+                else
+                    return RedirectToAction("Index");
+            }
+        }
+    }
+}
+    
