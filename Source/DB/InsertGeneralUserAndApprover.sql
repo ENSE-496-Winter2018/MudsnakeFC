7ab86ebe9ecb15c2d1979aa89d6ccde17ee91a09
zainchowdhary <zain-chowdhary@hotmail.com>
2018-10-09 17:51:57 -0600

    Add files via upload
    2 initial users for the table

---
+++ Source/DB/InsertGeneralUserAndApprover.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO [dbo].[User]
+           ([Username],[Hashed_password],[Type])
+     VALUES
+           ('496' ,'pwd','user')
+INSERT INTO [dbo].[User]
+           ([Username],[Hashed_password],[Type])
+     VALUES
+           ('496Approver' ,'pwd','approver')
+
